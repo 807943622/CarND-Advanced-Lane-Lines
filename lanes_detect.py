87e60e24c312149b024d78423cc31ec52b1c1b5b
Esteve Soria <esofabian@gmail.com>
2017-05-03 19:39:35 +0200

    Perspective transformation

--- lanes_detect.py
+++ lanes_detect.py
@@ -6,6 +6,8 @@ import glob
 import pickle
 import os.path
 
+import skvideo.io
+
 def calibrate_camera():
     images = glob.glob('./camera_cal/calibration*.jpg')
     ret, mtx, dist, rvecs, tvecs = 0, 0, 0, 0, 0
@@ -46,9 +48,39 @@ def test_calibration_camera():
         plt.show()
         plt.pause(0.5)
 
-def process_image():
-    pass
+def process_image(image, thresh=(85,255)):
+    hls = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+    #H = hls[:,:,0]
+    #L = hls[:,:,1]
+    S = hls[:,:,2]
+    binary = np.zeros_like(S)
+    binary[(S > thresh[0]) & (S <= thresh[1])] = 1
+    res = binary * 255
+    return res
 
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    """
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)   
+    
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+        
+    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
 
 if(os.path.exists('ret.pkl') and os.path.exists('mtx.pkl') and 
     os.path.exists('dist.pkl') and os.path.exists('rvecs.pkl') and os.path.exists('tvecs.pkl')):
@@ -65,3 +97,43 @@ else:
     print('Camera calibrated')
 
 #test_calibration_camera()
+def process_video(video):
+    vid = skvideo.io.vreader(video)
+    writer = skvideo.io.FFmpegWriter('video_output.mp4', verbosity=1)
+    for frame in vid:
+        img = process_image(frame)
+        img = np.expand_dims(img, axis=-1)
+#        plt.imshow(img)
+#        plt.pause(0.01)
+        writer.writeFrame(img)
+    writer.close()
+ 
+images = glob.glob('./test_images/*.jpg')
+
+for f in images:
+    img = cv2.imread(f)
+    #processed = process_image(img)
+    print(img.shape)
+    left_down_corner = 0.05*img.shape[0]
+    rigth_down_corner = 0.95*img.shape[0]
+    horizont = 0.6*img.shape[1]
+    horizont_margin = 0.07*img.shape[0]
+    middle = img.shape[0]/2
+    vertices = np.array([[(left_down_corner,img.shape[1]), 
+              (middle-horizont_margin, horizont), 
+              (middle + horizont_margin, horizont), 
+              (rigth_down_corner, img.shape[1]) ]], dtype=np.int32)
+    
+    src_points = np.float32([[200,720], [625,430], [660,430], [1110,720]])
+    dst_points = np.float32([[400,720], [400,0], [910,0], [910,720]])
+    cv2.line(img, (200,720), (625,430), [255,0,0], 2)
+    cv2.line(img, (1110,720), (660,430), [0,255,0], 2)
+    M = cv2.getPerspectiveTransform(src_points, dst_points)
+    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]), flags=cv2.INTER_LINEAR)
+    fig = plt.figure()
+    a = fig.add_subplot(1,2,1)
+    imgplot = plt.imshow(img)
+    a = fig.add_subplot(1,2,2)
+    imgplot = plt.imshow(warped)
+    plt.show()
+    #plt.pause(10)
